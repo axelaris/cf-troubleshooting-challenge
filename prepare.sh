06f130fac0a7c751e15ab598453ba55aa6788bb4
Konstantin Burtsev <konstantin.burtsev@altoros.com>
2019-02-28 14:15:20 +0300

    fixed find depth option

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@ set +x
 echo -n "Preparing Lab environment... "
 for i in `find . -type d`; do
 	pushd $i >/dev/null
-	find . -d 1 -name *tgz | xargs -I % tar -xzf %
+	find . -maxdepth 1 -name *tgz | xargs -I % tar -xzf %
 	popd >/dev/null
 done
 echo "Done"

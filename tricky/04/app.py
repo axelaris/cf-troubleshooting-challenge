d784bc1c29e9d864ba6d13e5475c09e96f48ef2f
Alexey Zakharov <alexey.zakharov@altoros.com>
2019-03-09 16:02:43 +0300

    Added congrats banner

--- tricky/04/app.py
+++ tricky/04/app.py
@@ -14,6 +14,9 @@ current_time = now.strftime("%H:%M")
 a = np.arange(15).reshape(3, 5)
 
 while True:
+    print('CONGRATULATIONS')
+    print('you just completed tricky task #04')
+    print('---')
     print('Current date and time', current_date, current_time)
     print(a)
     time.sleep(60)
